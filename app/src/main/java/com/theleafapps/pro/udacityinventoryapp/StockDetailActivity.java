6617f83138fac7c52c8b65c04f9caa739ef456f4
The Aviator <abhi@xyz>
2018-01-22 02:32:55 +0530

    Created menu in the detail activity Created full layout of stock detail activity Added Menu button image for saving inventory data for all densities Added image placeholder for inventory image for all densities

--- app/src/main/java/com/theleafapps/pro/udacityinventoryapp/StockDetailActivity.java
+++ app/src/main/java/com/theleafapps/pro/udacityinventoryapp/StockDetailActivity.java
@@ -2,6 +2,8 @@ package com.theleafapps.pro.udacityinventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StockDetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class StockDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_stock_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
